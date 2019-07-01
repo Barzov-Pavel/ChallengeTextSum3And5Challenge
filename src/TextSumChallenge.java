711870647d9c91d537c8ccaf7e6261e07121fe59
Barzov-Pavel <barzov_pavel@mail.ru>
2019-07-01 11:21:38 +0300

    TextSumChallenge

---
+++ src/TextSumChallenge.java
@@ -0,0 +1,19 @@
+public class TextSumChallenge {
+    public static void main(String[] args) {
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 1; i <= 1000; i++) {
+            if ((i % 3) == 0 && (i % 5 == 0)) {
+                count++;
+                sum += i;
+                System.out.println(i + " divide by 3 and 5");
+            }
+            if (count == 5) {
+                break;
+            }
+        }
+
+        System.out.println("Sum of number = " + sum);
+    }
+}
